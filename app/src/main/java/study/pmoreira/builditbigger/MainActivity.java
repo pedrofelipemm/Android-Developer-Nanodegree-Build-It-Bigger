8c0f22919d2c4cc1c8870084994e3ef7c7b1765b
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2017-04-29 16:22:50 -0300

    Initial commit

---
+++ app/src/main/java/study/pmoreira/builditbigger/MainActivity.java
@@ -0,0 +1,40 @@
+package study.pmoreira.builditbigger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void tellJoke(View view) {
+        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+    }
+
+}
