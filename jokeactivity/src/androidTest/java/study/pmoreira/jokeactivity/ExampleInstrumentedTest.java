0026183ba72c603440b533bd912f2b8a58c51658
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2017-04-29 19:24:42 -0300

    Step 2: Create an Android Library

---
+++ jokeactivity/src/androidTest/java/study/pmoreira/jokeactivity/ExampleInstrumentedTest.java
@@ -0,0 +1,22 @@
+package study.pmoreira.jokeactivity;
+
+import android.content.Context;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("study.pmoreira.jokeactivity.test", appContext.getPackageName());
+    }
+}
